c5b5733bdd2743a0c542afb3ddcf6f5f2f0a3826
castillouparela <89260570+castillouparela@users.noreply.github.com>
2022-09-11 17:17:03 -0500

    Delete src directory

--- src/main/java/integracion/proyectogradle/dao/ICategoriaDAO.java
+++
@@ -1,7 +0,0 @@
-package integracion.proyectogradle.dao;
-
-import integracion.proyectogradle.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICategoriaDAO extends JpaRepository<Categoria,Long> {
-}
